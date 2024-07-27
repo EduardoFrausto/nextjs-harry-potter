cf5f76f9e0d887a66a6bc373ad350869592b9177
EduardoFrausto <eduardo.dx98@gmail.com>
2024-07-27 15:21:40 -0600

    Header and fonts
    Place logo, header and set global font to open sans

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import BackgroundImage from "@/components/backgroundImage/BackgroundImage";
+import Image from "next/image";
+import {Open_Sans} from 'next/font/google'
+import logo from '../../public/svg/logo.svg'
+import styles from './page.module.css'
+
+const openSans = Open_Sans({subsets: ['latin']})
 
 export default function Home() {
     return (
-        <main>
+        <main className={openSans.className}>
             <BackgroundImage/>
+            <div className={styles.headerContainer}>
+                <Image src={logo} alt={'Harry Potter Logo'}/>
+                <h1 className={styles.headerTitle}>Selecciona tu filtro</h1>
+            </div>
         </main>
     );
 }
